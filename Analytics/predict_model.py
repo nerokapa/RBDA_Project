7415a8c8c6c8786b84d9aefce49cef04834059a1
Huri Ma <huri.ma@nyu.edu>
2017-12-10 23:19:48 -0500

    adding training script

--- Analytics/predict_model.py
+++ Analytics/predict_model.py
@@ -2,6 +2,8 @@ import os, sys
 import argparse
 from math import log10
 import ConfigParser
+import json
+
 from pyspark import SparkContext
 from pyspark.mllib.linalg import Vectors
 from pyspark.mllib.regression import LabeledPoint
@@ -11,6 +13,10 @@ from pyspark.mllib.tree import GradientBoostedTrees, GradientBoostedTreesModel
 from pyspark.mllib.tree import RandomForest, RandomForestModel
 from pyspark.mllib.util import MLUtils
 from pyspark.mllib.stat import Statistics
+
+from data_integration import all_movies
+
+sc = SparkContext('local', 'prediction_model')
 
 def model_factory(training_data, args, section, config, categorical_features_info={}):
     model = None
@@ -28,9 +34,11 @@ def model_factory(training_data, args, section, config, categorical_features_inf
         model = GradientBoostedTrees.trainRegressor(training_data,
                                             categoricalFeaturesInfo=categorical_features_info, numIterations=numIterations,
                                             loss=loss , maxDepth=maxDepth, maxBins=maxBins)
+    else:
+        raise Exception('Illeagle model')
     return model
 
-
+# this function will not be used in the future
 def parse(lp):
     # label = float(lp[ : lp.find(' ')])
     # vec = Vectors.dense(lp[lp.find(' ') + 1: ].split(' '))
@@ -39,9 +47,18 @@ def parse(lp):
          return LabeledPoint(-1, [0] * (len(values) - 1))
     # import pdb; pdb.set_trace()
     return LabeledPoint(values[0], [values[1 : ]])
-    # return LabeledPoint(values[0], [values[1]])
+    # return LabeledPoint(values[0], [values[1]]
 
-def evaluation(model, test_data, verbose=False, output=None):
+# def parse_oa_statistic(lp):
+#     lp = json.loads(lp)
+#     map(lambda x : lp.pop(x), ['oa_screen_name', 'movie_name'])
+#     movie_id = lp.pop('movie_id')
+#     lp = lp.items().sort(key=lambda tup: tup[0])
+#     lp = map(lambda tup : tup[1], lp)
+#     return (movie_id,lp);
+
+
+def evaluation(section, model, test_data, verbose=False, output=None):
     predictions = model.predict(test_data.map(lambda x: x.features))
     gold = test_data.map(lambda x : x.label)
     labelsAndPredictions = test_data.map(lambda lp: lp.label).zip(predictions)
@@ -49,6 +66,7 @@ def evaluation(model, test_data, verbose=False, output=None):
                                                             float(labelsAndPredictions.count())
     test_error_sqaure_mean= labelsAndPredictions.map(lambda lp: (lp[0] - lp[1]) * (lp[0] - lp[1])).sum() /\
                                                             float(labelsAndPredictions.count())
+    print("evaluating the section: " + section)
     print("THe pearson corrolation equals:")
     print(Statistics.corr(labelsAndPredictions, method="pearson"))
     print('Test Mean Squared Error = ' + str(test_error_sqaure_mean))
@@ -63,56 +81,80 @@ def evaluation(model, test_data, verbose=False, output=None):
                 with open(output, 'a+') as fout:
                     fout.write(str(item) + '\n')
 
-# trainingData = ssc.textFileStream("/training/data/dir").map(parse).cache()
-# testData = ssc.textFileStream("/testing/data/dir").map(parse)
+def Prep_data_hbase(args):
+    tmdb_data = []
+    for item in all_movies():
+        features = [item['budget']] + [item['cast_impression']] + [item['year']]
+        features = features + item['genre'] + item['lang']
+        t = ()
+        t = t +  (item['id'], )
+        t = t + ([item['revenue']] + features, )
+        tmdb_data.append(t)
 
-def Prep_data_data_base():
+    # if args.twitter_file:
+    #     oa_stat_data = sc.textFile(args.twitter_file)
+    #     oa_stat_data = oa_stat_data.map(parse_oa_statistic)
+    #     oa_stat_data.join(tmdb_data)
+    #
+    #     all_data = all_d11ata.values()
+    # else:
+    #     all_data = tmdb_data.values()
+
+
+    #split all_feature data
     pass
 
-def Prep_data_file(args, verbose=False):
+
+
+def Prep_data_file(args):
     data = sc.textFile(args.data)
     parsed_data = data.map(parse)
     parsed_data = parsed_data.filter(lambda x : x.label > 0)
-    if verbose:
+    if args.verbose:
         parsed_data_mem = parsed_data.collect()
         for line in parsed_data_mem:
             print(str(line) + '\n')
-    # random split is problematic
     training_data, val_data, test_data = parsed_data.randomSplit([0.7, 0.15, 0.15])
     return training_data, val_data, test_data
+
 
 def get_cata_dict(config):
     res = {}
     for x in range(config.getint('data', 'cate_range_begin'),
                     config.getint('data', 'cate_range_end')):
         res[x] = 2
-    res[config.getint('data', 'year_index')] = config.getint('data', 'year_max') - \
-                                               config.getint('data', 'year_min') + 1
     return res
 
 
-sc = SparkContext('local', 'prediction_model')
 
-# test_ratio = 0.2;
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='ML model for predicting the box offic')
-    parser.add_argument('-data', dest="data", required=True,
+    parser.add_argument('-data', dest="data", default=None,
                         help = "The file that contain all the data")
+    parser.add_argument('-twitter', dest='twitter_file', default=None,
+                        help = 'The file to twitter official account statistics')
     parser.add_argument('-o', dest='output', default=None,
                         help = "Output file path")
     parser.add_argument('-mode', dest='mode', default='train',
                         help = "model running mode default: train or test")
     parser.add_argument('-config', dest='cfg_file', default='model_config.cfg',
                         help = "model configer file path")
-    parser.add_argument('-model', dest='model_path', default=None)
-    parser.add_argument('-v', dest='verbose', action='store_true')
+    parser.add_argument('-model', dest='model_path', default=None,
+                        help = "the path to save or load model")
+    parser.add_argument('-v', dest='verbose', action='store_true',
+                        help = 'use this option to set the flag of verbose mode')
 
     args = parser.parse_args()
     config = ConfigParser.ConfigParser()
     config.read(args.cfg_file)
 
-    training_data, val_data, test_data = Prep_data_file(args)
+    # loading from data is just for testing the function
+    if args.data:
+        training_data, val_data, test_data = Prep_data_file(args)
+    else:
+        training_data, val_data, test_data = Prep_data_hbase(args)
 
     if args.mode == 'train':
         for section in config.sections():
@@ -122,7 +164,7 @@ if __name__ == "__main__":
                           get_cata_dict(config))
             if config.getboolean(section, 'save'):
                 model.save(sc, args.model_path + section)
-            evaluation(model, val_data, args.verbose, output=args.output + section)
+            evaluation(section, model, val_data, args.verbose, output=args.output + section)
 
     elif args.mode == 'test':
         for section in config.sections():
@@ -134,4 +176,4 @@ if __name__ == "__main__":
                 model = GradientBoostedTreesModel.load(sc, output=args.output + section)
             else:
                 raise Exception('Illeagle model')
-            evaluation(model, test_data, args.verbose)
+            evaluation(section, model, test_data, args.verbose)
---
+++ Analytics/train.sh
@@ -0,0 +1,4 @@
+#!bash
+
+python predict_model.py -config model_config.cfg -data ../twitter_based_predict/Regression/data/BudgetCastYearGenreLang.dat \
+               -o ../Result/outputs/train_1 -model ../Result/models/tr_1
