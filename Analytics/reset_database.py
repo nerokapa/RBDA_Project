87f6057742121a0506b28b7375dce179ed62df76
Huri Ma <huri.ma@nyu.edu>
2017-12-11 21:29:21 -0500

    debug resetdatabase

--- Analytics/reset_database.py
+++ Analytics/reset_database.py
@@ -1,9 +1,9 @@
 #encoding=utf-8
 
-from hbase_connection import reset_database
+from hbase_connection import *
 
 if __name__ == "__main__":
+    reset_database()
     insert_movie_data()
     insert_cast_data()
     post_process()
-    reset_database()
