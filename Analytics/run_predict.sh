1abac0ad3a4e81283d82974a12bbad60663aec2b
HuoDa <dh2582@nyu.edu>
2017-12-12 22:34:19 -0500

    add used data

--- Analytics/run_predict.sh
+++ Analytics/run_predict.sh
@@ -1,4 +1,4 @@
 #!bash
 
 python predict_model.py -config model_config.cfg -file_buffer roam -strip \
-                       -o res/UsingTwitter.dat -model res/model -twitter ../Data/oa_statistic.json
+                       -o res/UsingTwitter.dat -model res/model -twitter ../Data/twitter_data.json
